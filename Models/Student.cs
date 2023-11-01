ffac76698ebeb78b89b020063eb4151ea371f784
Teodora-Derek <127680962+Teodora-Derek@users.noreply.github.com>
2023-11-01 09:25:40 +0200

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,100 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OnlineCompilerWebApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid FacultyNumber { get; set; }
+
+        [Required]
+        [NineDigitNumeric]
+        [Column(TypeName = "nvarchar(10)")]
+        public string? FacultyName { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(FirstName))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(MiddleName))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string MiddleName { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(LastName))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(AcademicDegreeName))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string AcademicDegreeName { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(Email))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public DegreeType AcademicDegreeType { get; set; }
+
+        [Required]
+        public short Stream { get; set; }
+
+        [Required]
+        public short Group { get; set; }
+
+        [Required]
+        public short Semester { get; set; }
+
+        [Required]
+        public short Course { get; set; }
+
+        [Required]
+        public StudyType StudyType { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+    }
+
+    public enum DegreeType
+    {
+        Bachelor,
+        Master
+    }
+
+    public enum StudyType
+    {
+        FullTime,
+        PartTime
+    }
+
+    public enum Status
+    {
+        Active,
+        Disabled
+    }
+
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class NineDigitNumericAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string facultyNumber)
+            {
+                if (facultyNumber.Length == 9 && facultyNumber.All(char.IsDigit))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return new ValidationResult("FacultyNumber must be a 9-digit string containing only numeric digits.");
+        }
+    }
+}
---
+++ Models/StudentContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCompilerWebApplication.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        { }
+
+        //Table Name Students
+        public DbSet<Student> Students { get; set; }
+    }
+}
